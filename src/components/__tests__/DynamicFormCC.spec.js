be5926f2aa9555dd296e2ee6e8112bd88c4f36e6
Daniel Bail <Daniel.Bail@Student.HTW-Berlin.de>
2024-01-11 17:40:53 +0100

    Added test

--- src/components/__tests__/DynamicFormCC.spec.js
+++ src/components/__tests__/DynamicFormCC.spec.js
@@ -1,13 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
-import DynamicFormCC from '@/components/DynamicFormCC.vue';
-
-// eslint-disable-next-line no-undef
-vi.stubGlobal('fetch', vi.fn(() =>
-    Promise.resolve({
-        json: () => Promise.resolve([])
-    })
-));
+import DynamicFormCC from '@/components/DynamicFormCC.vue'; // Pfad anpassen
 
 describe('DynamicFormCC', () => {
     it('sollte die Komponente korrekt erstellen', () => {
